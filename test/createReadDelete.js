6c80408416cf3254b52296a8db6f39a14a58cd12
Márcio Zampiron <43278442+marciozampiron@users.noreply.github.com>
2025-06-08 16:36:06 -0300

    Update createReadDelete.js

--- test/createReadDelete.js
+++ test/createReadDelete.js
@@ -1,51 +1,32 @@
-var assert = require('assert');
-var request = require('request');
-var fs = require('fs');
+const assert = require('assert');
+const request = require('request');
 
 describe('Create, Read, Delete', function() {
-	this.timeout(5000);
-    it('should create a new Todo, read it, & delete it', function(done) {
-		// Build and log the path
-		var path = "https://" + process.env.TODOS_ENDPOINT + "/todos";
+  this.timeout(5000);
+  it('should create a new Todo, read it, & delete it', function(done) {
+    const base = `https://${process.env.TODOS_ENDPOINT}/todos`;
+    const desiredPayload = require('./data/newTodo1.json');
 
-		// Fetch the comparison payload
-		require.extensions['.txt'] = function (module, filename) {
-		    module.exports = fs.readFileSync(filename, 'utf8');
-		};
-		var desiredPayload = require("./data/newTodo1.json");
-		
-		// Create the new todo
-		var options = {'url' : path, 'form': JSON.stringify(desiredPayload)};
- 		request.post(options, function (err, res, body){ 
-			if(err){
-				throw new Error("Create call failed: " + err);
-			}
-			assert.equal(200, res.statusCode, "Create Status Code != 200 (" + res.statusCode + ")");
-			var todo = JSON.parse(res.body);
-			// Read the item
-			var specificPath = path + "/" + todo.id;
-			request.get(path, function (err, res, body){ 
-				if(err){
-					throw new Error("Read call failed: " + err);
-				}
-				assert.equal(200, res.statusCode, "Read Status Code != 200 (" + res.statusCode + ")");
-				
-				var todoList = JSON.parse(res.body);
-				if(todoList.text = desiredPayload.text)	{
-					// Item found, delete it
-		 			request.del(specificPath, function (err, res, body){ 
-						if(err){
-							throw new Error("Delete call failed: " + err);
-						}
-						assert.equal(200, res.statusCode, "Delete Status Code != 200 (" + res.statusCode + ")"); 
-						done();   
-		  			});
-				} else {
-					// Item not found, fail test
-					assert.equal(true, false, "New item not found in list."); 
-					done();
-				} 
-			}); 	
-  		});
+    // Create the new todo
+    request.post({ url: base, json: desiredPayload }, (err, res, body) => {
+      if (err) throw new Error(`Create call failed: ${err}`);
+      assert.strictEqual(res.statusCode, 200, `Create Status Code != 200 (${res.statusCode})`);
+      const id = body.id;
+
+      // Read the item
+      const specificUrl = `${base}/${id}`;
+      request.get({ url: specificUrl, json: true }, (err, res, todo) => {
+        if (err) throw new Error(`Read call failed: ${err}`);
+        assert.strictEqual(res.statusCode, 200, `Read Status Code != 200 (${res.statusCode})`);
+        assert.strictEqual(todo.text, desiredPayload.text, 'Read text != desired payload');
+
+        // Delete the item
+        request.del(specificUrl, (err, res) => {
+          if (err) throw new Error(`Delete call failed: ${err}`);
+          assert.strictEqual(res.statusCode, 200, `Delete Status Code != 200 (${res.statusCode})`);
+          done();
+        });
+      });
     });
+  });
 });
